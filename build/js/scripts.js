065e64efedb1c29dc65107622b93725cb5ec1d05
Olga Semenova <olga110723@gmail.com>
2023-11-01 16:26:28 +0300

    feat: read more js privacy

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,3 +1,19 @@
 // Custom Scripts
 // Custom scripts
 
+window.onload = function () {
+    let buttons = document.querySelectorAll('.privacy__block-link');
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            let text = button.previousElementSibling;
+            if (text.classList.contains('show-text')) {
+                text.classList.remove('show-text');
+                button.textContent = 'читать далее';
+            } else {
+                text.classList.add('show-text');
+                button.textContent = 'свернуть';
+            }
+        });
+    });
+};
+
